e099e21f8e4714ad5baab952ace386dc36a948e3
rizalazky <rizalazky26@gmail.com>
2021-02-26 02:45:13 +0700

    koneksi

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,20 +1,27 @@
 <?php
 
 // panggil fungsi validasi xss dan injection
+
 require_once('fungsi_validasi.php');
 
 $server   = "localhost:3306";
-$username = "root";
-$password = "";
-$database = "svnvbkdl_dbapemasi";
-$bpsda    = "BPSDA PEMALI COMAL";
-$lbpsda   = "Balai SDA Pemali Comal";
-$lokasi   = "Tegal";
-$web      = "https://pemalicomal.com";
+$username = "bodrikut_apemasi";
+$password = "123Februari";
+$database = "bodrikut_apemasi";
+$bpsda    = "BPUSDATARU BODRI KUTO";
+$lbpsda   = "BALAI PU SDA TATA RUANG BODRI KUTO";
+$lokasi   = "Semarang";
+$web      = "http://www.bodrikuto.com/";
 
 // Koneksi dan memilih database di server
-mysql_connect($server,$username,$password) or die("Koneksi gagal");
-mysql_select_db($database) or die("Database tidak bisa dibuka");
+if(mysql_connect($server,$username,$password)){
+
+    mysql_select_db($database) or die("Database tidak bisa dibuka".mysql_error());
+}else{
+    die("Koneksi gagal");
+}
+
+
 
 // buat variabel untuk validasi dari file fungsi_validasi.php
 $val = new Psda;
