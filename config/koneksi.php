7177a41cef311e689bd251def7fdff1a73fb3e01
rizalazky <rizalazky26@gmail.com>
2021-02-26 02:51:06 +0700

    test

--- config/koneksi.php
+++ config/koneksi.php
@@ -18,7 +18,7 @@ if(mysql_connect($server,$username,$password)){
 
     mysql_select_db($database) or die("Database tidak bisa dibuka".mysql_error());
 }else{
-    die("Koneksi gagal");
+    die("Koneksi gagal".mysql_error());
 }
 
 
