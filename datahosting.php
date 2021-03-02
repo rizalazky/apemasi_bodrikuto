0fbcd9b14a449729c6cfa2ae30233166054aef7a
rizalazky <rizalazky26@gmail.com>
2021-03-02 23:31:48 +0700

    datahosting 5

--- datahosting.php
+++ datahosting.php
@@ -13,8 +13,8 @@ $Qki      = $_POST['Qki'];
 $auth     = $_POST['auth'];
 if ($auth=='2e422bed3131daf11e89016073b230cd'){
     $quey="INSERT INTO elevasi(kode,bendung,elevasi,debit,Qka,Qki,tgl,jam,status,petugas) VALUES('$kode','$bendung','$elevasi','$debit','$Qka','$Qki','$tgl','$jam','$status','$petugas')" ;
-    $smsquery="INSERT INTO inbox(TextDecoded,SenderNumber,ReceivingDateTime) VALUES ('$sms','$sendNomer','$receivingDatetime')";
-    if(mysql_query($quey) AND mysql_query($smsquery)){
+    $smsquery="INSERT INTO inbox(TextDecoded,SenderNumber,ReceivingDateTime) VALUES('$sms','$sendNomer','$receivingDatetime')";
+    if(mysql_query($quey) && mysql_query($smsquery)){
         echo "Berhasi;";
     }else{
         echo "gagal".mysql_error();
